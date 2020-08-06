7f55929330bdc95ea05f6f45663f641c5d7033f4
olesya <olesya.wo@gmail.com>
2020-08-06 18:34:25 +0300

    settings fix

--- main.php
+++ main.php
@@ -3,7 +3,6 @@ chdir(__DIR__);
 require 'settings.php';
 
 function call_tg_api_method( $method, $params ): bool {
-    global $token;
     $post_data = http_build_query( $params );
     $opts     = array(
         'http' => array(
@@ -19,7 +18,7 @@ function call_tg_api_method( $method, $params ): bool {
             'verify_peer_name'  => false
         )
     );
-    $content = file_get_contents( 'https://api.telegram.org/bot' . $token . '/' . $method, false, stream_context_create( $opts ) );
+    $content = file_get_contents( 'https://api.telegram.org/bot' . BOT_TOKEN . '/' . $method, false, stream_context_create( $opts ) );
     if ( !$content ) { return false; }
     $data = null;
     try {
@@ -32,16 +31,15 @@ function call_tg_api_method( $method, $params ): bool {
 }
 
 function on_exit() {
-    global $lock_file;
-    unlink( $lock_file );
+    unlink( LOCK_FILE );
 }
 
-if ( file_exists( $lock_file ) && time() - filemtime( $lock_file ) < $max_lock_lifetime ) { die( 'Already running' ); }
+if ( file_exists( LOCK_FILE ) && time() - filemtime( LOCK_FILE ) < MAX_LOCK_TIME_SEC ) { die( 'Already running' ); }
 
 register_shutdown_function( 'on_exit' );
-touch( $lock_file );
+touch( LOCK_FILE );
 
-$last_posted_id = ( int ) file_get_contents( $last_id_file );
+$last_posted_id = ( int ) file_get_contents( LAST_ID_FILE );
 if ( $last_posted_id < 1 ) { die( 'No last post id' ); }
 
 $opts = array(
@@ -55,7 +53,7 @@ $opts = array(
         'verify_peer_name'  => false
     )
 );
-$content = file_get_contents( $thread_json, false, stream_context_create( $opts ) );
+$content = file_get_contents( THREAD_JSON, false, stream_context_create( $opts ) );
 if ( !$content ) { die( 'Getting thread content fail' ); }
 $data = null;
 try {
@@ -70,25 +68,25 @@ foreach ( $data->threads[0]->posts as $post ) {
     $num = $post->num;
     if ( $num <= $last_posted_id ) { continue; }
     
-    $link = "<a href=\"$thread_url#$num\">[$num]</a>\r\n";
+    $link = '<a href="' . THREAD_URL . "#$num\">[$num]</a>\r\n";
 
     $files = '';
     foreach( $post->files as $file ) {
-        $files .= $site_url . $file->path . "\r\n";
+        $files .= SITE_URL . $file->path . "\r\n";
     }
 
     $comment = strip_tags( str_replace( '<br>', "\r\n", $post->comment ), '<b><i><u><s><a><code><pre>' );
 
     $res = $link . $files . $comment;
 
-    if ( mb_strlen( $res ) > $message_maxlength ) {
-        $res = mb_substr( $res, 0, $message_maxlength ) . "\r\n[...]";
+    if ( mb_strlen( $res ) > MESSAGE_MAXLENGTH ) {
+        $res = mb_substr( $res, 0, MESSAGE_MAXLENGTH ) . "\r\n[...]";
     }
-    
+
     $send_res = call_tg_api_method(
         'sendMessage',
         [
-            'chat_id'                  => $channel_name,
+            'chat_id'                  => TG_CHANNEL_NAME,
             'text'                     => $res,
             'parse_mode'               => 'HTML',
             'disable_web_page_preview' => true
@@ -97,7 +95,7 @@ foreach ( $data->threads[0]->posts as $post ) {
 
     if ( !$send_res ) { die( 'call_tg_api_method fail' ); }
 
-    file_put_contents( $last_id_file, $num );
-    touch( $lock_file );
+    file_put_contents( LAST_ID_FILE, $num );
+    touch( LOCK_FILE );
     sleep( 4 );
 }
--- settings.example.php
+++ settings.example.php
@@ -1,11 +1,16 @@
 <?php
-$token             = ''; // telegram-bot token
-$channel_name      = '@channel_name';
-$site_url          = 'https://2ch.hk';
-$thread_number     = '4219284';
-$thread_json       = "{$site_url}/soc/res/{$thread_number}.json";
-$thread_url        = "{$site_url}/soc/res/{$thread_number}.html";
-$last_id_file      = "./data/last_post_for_{$thread_number}.txt";
-$lock_file         = "./data/thread_{$thread_number}_dump.lock";
-$message_maxlength = 4000;
-$max_lock_lifetime = 60 * 30; // 30m
+
+define( 'BOT_TOKEN', '' );
+define( 'TG_CHANNEL_NAME', '@channel_name' );
+define( 'MESSAGE_MAXLENGTH', 4000 );
+
+define( 'SITE_URL', 'https://2ch.hk' );
+define( 'BOARD', 'soc' );
+define( 'THREAD_NUMBER', '4219284' );
+define( 'THREAD_JSON', SITE_URL . '/' . BOARD . '/res/' . THREAD_NUMBER . '.json' );
+define( 'THREAD_URL', SITE_URL . '/' . BOARD . '/res/' . THREAD_NUMBER . '.html' );
+
+define( 'LAST_ID_FILE', './data/last_post_from_' . THREAD_NUMBER );
+
+define( 'LOCK_FILE', './data/thread_' . THREAD_NUMBER . '_dump.lock' );
+define( 'MAX_LOCK_TIME_SEC', 2000 );
